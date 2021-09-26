2a10a91d506f53e7fa200290b699f62e9d3e86f7
SavenkovaAV <90615021+SavenkovaAV@users.noreply.github.com>
2021-09-26 22:07:02 +0300

    Update Main.java

--- Task_1.2/src/ru/vsu/cs/savenkova_a_v/Main.java
+++ Task_1.2/src/ru/vsu/cs/savenkova_a_v/Main.java
@@ -7,32 +7,38 @@ import java.util.Scanner;
 //Вычислить высоты этого треугольника.
 
 public class Main {
-  
+
     public static void main (String[] args) {
         Locale.setDefault(Locale.ROOT);
 
-        Scanner scanner = new Scanner(System.in);
-
         System.out.println("Введите длины сторон треугольника:");
+        double x = 0;
         System.out.print("a=");
-        double a = scanner.nextDouble();
+        double a = readlength(x);
         System.out.print("b=");
-        double b = scanner.nextDouble();
+        double b = readlength(x);
         System.out.print("c=");
-        double c = scanner.nextDouble();
+        double c = readlength(x);
         double P = poluperimetr(a, b, c);
         double ha = heighta(a, b, c, P);
         double hb = heightb(a, b, c, P);
         double hc = heightc(a, b, c, P);
 
-        System.out.printf("ha = %.03f%n", ha);
-        System.out.printf("hb = %.03f%n", hb);
-        System.out.printf("hc = %.03f%n", hc);
-        
-         //Метод для нахождения полупериметра треугольника
+        answera(ha);
+        answerb(hb);
+        answerc(hc);
+    }
+    private static double readlength (double l) {
+        Scanner scanner = new Scanner(System.in);
+        l = scanner.nextDouble();
+        return l;
+        }
+  
+    //Метод для нахождения полупериметра треугольника
     private static double poluperimetr (double a, double b, double c) {
         return (a + b + c) / 2;
     }
+  
     //Методы для нахождения высот треугольника
     private static double heighta (double a, double b, double c, double P) {
         return (2 / a) * (Math.sqrt(P * (P - a) * (P - b) * (P - c)));
@@ -45,6 +51,17 @@ public class Main {
     private static double heightc (double a, double b, double c, double P) {
         return (2 / c) * (Math.sqrt(P * (P - a) * (P - b) * (P - c)));
     }
+
+    private static void answera (double ha) {
+        System.out.printf("ha = %.03f%n", ha);
+    }
+
+    private static void answerb (double hb) {
+        System.out.printf("hb = %.03f%n", hb);
+    }
+
+    private static void answerc (double hc) {
+        System.out.printf("hc = %.03f%n", hc);
     }
 }
 
