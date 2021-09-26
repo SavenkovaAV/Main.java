cd18680495d11318292ce8fff59cb7fd02de8e47
SavenkovaAV <90615021+SavenkovaAV@users.noreply.github.com>
2021-09-26 21:37:33 +0300

    Update Main.java

--- Task_1.2/src/ru/vsu/cs/savenkova_a_v/Main.java
+++ Task_1.2/src/ru/vsu/cs/savenkova_a_v/Main.java
@@ -7,23 +7,7 @@ import java.util.Scanner;
 //Вычислить высоты этого треугольника.
 
 public class Main {
-    //Метод для нахождения полупериметра треугольника
-    private static double poluperimetr (double a, double b, double c) {
-        return (a + b + c) / 2;
-    }
-    //Методы для нахождения высот треугольника
-    private static double heighta (double a, double b, double c, double P) {
-        return (2 / a) * (Math.sqrt(P * (P - a) * (P - b) * (P - c)));
-    }
-
-    private static double heightb (double a, double b, double c, double P) {
-        return (2 / b) * (Math.sqrt(P * (P - a) * (P - b) * (P - c)));
-    }
-
-    private static double heightc (double a, double b, double c, double P) {
-        return (2 / c) * (Math.sqrt(P * (P - a) * (P - b) * (P - c)));
-    }
-
+  
     public static void main (String[] args) {
         Locale.setDefault(Locale.ROOT);
 
@@ -44,6 +28,23 @@ public class Main {
         System.out.printf("ha = %.03f%n", ha);
         System.out.printf("hb = %.03f%n", hb);
         System.out.printf("hc = %.03f%n", hc);
+        
+         //Метод для нахождения полупериметра треугольника
+    private static double poluperimetr (double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+    //Методы для нахождения высот треугольника
+    private static double heighta (double a, double b, double c, double P) {
+        return (2 / a) * (Math.sqrt(P * (P - a) * (P - b) * (P - c)));
+    }
+
+    private static double heightb (double a, double b, double c, double P) {
+        return (2 / b) * (Math.sqrt(P * (P - a) * (P - b) * (P - c)));
+    }
+
+    private static double heightc (double a, double b, double c, double P) {
+        return (2 / c) * (Math.sqrt(P * (P - a) * (P - b) * (P - c)));
+    }
     }
 }
 
